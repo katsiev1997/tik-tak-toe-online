9b055f1d9fc5d251336baad3942dbcd96153acec
Mikail Katsiev <105316523+katsiev1997@users.noreply.github.com>
2024-11-29 17:43:14 +0300

    feat prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,15 @@ import { prisma } from "@/shared/lib/db";
 import { Card, CardTitle } from "@/shared/ui/card";
 
 export default async function Home() {
-    const games = await prisma.game.findMany();
+  const games = await prisma.game.findMany();
 
-    return (
-        <div>
-            {games.map((game) => (
-                <Card key={game.id}>
-                    <CardTitle>{game.name}</CardTitle>
-                </Card>
-            ))}
-        </div>
-    );
+  return (
+    <div>
+      {games.map((game) => (
+        <Card key={game.id}>
+          <CardTitle>{game.name}</CardTitle>
+        </Card>
+      ))}
+    </div>
+  );
 }
